a729584411d45991d9560ffd5b7bc5590e651618
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-12-26 14:49:42 -0500

    Adjusted style of dialog component

--- app/_components/Dialog.tsx
+++ app/_components/Dialog.tsx
@@ -36,21 +36,22 @@ const Dialog: React.FC<DialogProps> = ({ title, onClose, onOk, children }) => {
 
     const dialog: JSX.Element | null = showDialog === 'y'
         ? (
-            <dialog ref={dialogRef}
-                className="card fixed top-50 left-50 -translate-x-50 -translate-y-50 
-            z-10  rounded-xl backdrop:bg-gray-800/50">
-                <div>
-                    <div>
-                        <h3 className="card-title">{title}</h3>
+
+            <dialog ref={dialogRef} className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10  backdrop:bg-gray-800/50 bg-white rounded-xl">
+                <div className="w-[500px] max-w-fullbg-gray-200 flex flex-col">
+                    <div className="flex flex-row justify-between mb-4 pt-2 px-5 bg-primary">
+                        <h1 className="text-2xl text-white">{title}</h1>
                         <button
                             onClick={closeDialog}
+                            className="mb-2 py-1 px-2 cursor-pointer rounded border-none w-8 h-8 font-bold bg-red-600 text-white"
                         >x</button>
                     </div>
-                    <div>
+                    <div className="px-5 pb-6">
                         {children}
-                        <div>
+                        <div className="flex flex-row justify-end mt-2">
                             <button
                                 onClick={clickOk}
+                                className="bg-green-500 py-1 px-2 rounded border-none"
                             >
                                 OK
                             </button>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,26 +6,40 @@ import PageTitle from '../_components/PageTitle'
 import Dialog from "../_components/Dialog"
 
 export default function Dashboard() {
+    async function onClose() {
+        "use server"
+        console.log('closed')
+    }
+    async function onOk() {
+        "use server"
+        console.log('ok was clicked')
+    }
 
     return (
+        <>
+            <div className='ml-sidebar-width'>
+                <Dialog title="Add Assignment" onClose={onClose} onOk={onOk} >
+                    <p>
+                        lorem ipsum delor sit amet consectueur
+                    </p>
+                </Dialog>
+                <PageTitle title='Dashboard' />
+                <div className='flex p-4 gap-4'>
+                    <div className='basis-1/3 '>
+                        <AssignmentsList showAddAssignment={true} />
+                    </div>
+                    <div className='basis-1/3'>
+                        <ThisWeek />
+                    </div>
+                    <div className='basis-1/3 flex flex-col gap-4'>
+                        <MiniCalender />
+                        <ExamsList />
+                    </div>
 
-        <div className='ml-sidebar-width'>
-            <PageTitle title='Dashboard' />
-            <div className='flex p-4 gap-4'>
-                <div className='basis-1/3 '>
-                    <AssignmentsList showAddAssignment={true} />
-                </div>
-                <div className='basis-1/3'>
-                    <ThisWeek />
-                </div>
-                <div className='basis-1/3 flex flex-col gap-4'>
-                    <MiniCalender />
-                    <ExamsList />
                 </div>
 
-            </div>
-
-        </div >
+            </div >
+        </>
     )
 }
 
