8abb36c066a17746e04bc06e6028367e577b6d06
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-12-28 00:50:21 -0500

    installed firebase sdk and added a firebase.ts file to the services folder

---
+++ services/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyADvSy9F57h5ztEUsdl_w8XiasNlCHdTz0",
+    authDomain: "assignmint-63f8b.firebaseapp.com",
+    projectId: "assignmint-63f8b",
+    storageBucket: "assignmint-63f8b.appspot.com",
+    messagingSenderId: "908260062635",
+    appId: "1:908260062635:web:80ec4d473546097c190eee",
+    measurementId: "G-LE7VXB2MFS",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
