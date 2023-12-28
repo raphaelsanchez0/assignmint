3cb8118a5b33af16a7f1631892822eb95fec0132
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-12-28 00:38:47 -0500

    used mapping in order to make data fetching from api easier in the assignments list component in the dashboard

--- app/_components/AssignmentsList/AssignmentsList.tsx
+++ app/_components/AssignmentsList/AssignmentsList.tsx
@@ -1,54 +1,140 @@
+"use client";
+
 import Link from "next/link";
 import Assignment from "./Assignment";
 import SectionDivider from "./SectionDivider";
+
+import { useState } from "react";
 
 interface AssignmentsListProps {
     showAddAssignment?: boolean;
 }
 
-const AssignmentsList: React.FC<AssignmentsListProps> = ({ showAddAssignment = false }) => {
+const AssignmentsList: React.FC<AssignmentsListProps> = ({
+    showAddAssignment = false,
+}) => {
+    const [assignments, setAssignments] = useState([
+        {
+            name: "Pogram 10",
+            course: "CS 1114",
+            dueDate: "Nov 12",
+            color: "#DB5F5F",
+            category: "overdue",
+        },
+        {
+            name: "Reading 2",
+            course: "ITDS 2064",
+            dueDate: "Nov 12",
+            color: "#EFEA6A",
+            category: "overdue",
+        },
+        {
+            name: "Essay 3",
+            course: "ENGL 2122",
+            dueDate: "Nov 12",
+            color: "#5F89DB",
+            category: "overdue",
+        },
+        {
+            name: "Pogram 10",
+            course: "CS 1114",
+            dueDate: "Nov 12",
+            color: "#DB5F5F",
+            category: "priority",
+        },
+        {
+            name: "Essay 3",
+            course: "ENGL 2122",
+            dueDate: "Nov 12",
+            color: "#5F89DB",
+            category: "priority",
+        },
+        {
+            name: "Essay 3",
+            course: "ENGL 2122",
+            dueDate: "Nov 12",
+            color: "#5F89DB",
+            category: "due today",
+        },
+        {
+            name: "Pogram 10",
+            course: "CS 1114",
+            dueDate: "Nov 12",
+            color: "#DB5F5F",
+            category: "priority",
+        },
+        {
+            name: "Essay 3",
+            course: "ENGL 2122",
+            dueDate: "Nov 12",
+            color: "#5F89DB",
+            category: "priority",
+        },
+        {
+            name: "Essay 3",
+            course: "ENGL 2122",
+            dueDate: "Nov 12",
+            color: "#5F89DB",
+            category: "due today",
+        },
+    ]);
+
+    const [overdueAssignments, setOverdueAssignments] = useState(
+        assignments.filter((assignment) => assignment.category === "overdue"),
+    );
+    const [priorityAssignments, setPriorityAssignments] = useState(
+        assignments.filter((assignment) => assignment.category === "priority"),
+    );
+    const [dueTodayAssignments, setDueTodayAssignments] = useState(
+        assignments.filter((assignment) => assignment.category === "due today"),
+    );
+
     return (
         <div className="card">
             <div className="flex items-center justify-between">
                 <h3 className="card-title">Assignments</h3>
-                {
-                    showAddAssignment &&
-
+                {showAddAssignment && (
                     <Link href="/dashboard?addassignment=y">
                         <button className="btn">Add</button>
                     </Link>
-
-                }
-
+                )}
             </div>
-            <div>
+            <ol>
                 <SectionDivider title="Overdue" color="#FC7C7C" />
-                <Assignment name="Pogram 10" course="CS 1114"
-                    due="Nov 12" color="#DB5F5F" />
-                <Assignment name="Reading 2" course="ITDS 2064"
-                    due="Nov 12" color="#EFEA6A" />
-                <Assignment name="Essay 3" course="ENGL 2122"
-                    due="Nov 12" color="#5F89DB" />
-                <SectionDivider title="Priority" color="#F4BD6B" />
-                <Assignment name="Pogram 10" course="CS 1114"
-                    due="Nov 12" color="#DB5F5F" />
-                <Assignment name="Essay 3" course="ENGL 2122"
-                    due="Nov 12" color="#5F89DB" />
-                <SectionDivider title="Due Today" color="#7DC672" />
-                <Assignment name="Essay 3" course="ENGL 2122"
-                    due="Nov 12" color="#5F89DB" />
-                <SectionDivider title="Priority" color="#F4BD6B" />
-                <Assignment name="Pogram 10" course="CS 1114"
-                    due="Nov 12" color="#DB5F5F" />
-                <Assignment name="Essay 3" course="ENGL 2122"
-                    due="Nov 12" color="#5F89DB" />
-                <SectionDivider title="Due Today" color="#7DC672" />
-                <Assignment name="Essay 3" course="ENGL 2122"
-                    due="Nov 12" color="#5F89DB" />
-            </div>
 
+                {overdueAssignments.map((assignment) => (
+                    <Assignment
+                        key={assignment.name}
+                        name={assignment.name}
+                        course={assignment.course}
+                        due={assignment.dueDate}
+                        color={assignment.color}
+                    />
+                ))}
+
+                <SectionDivider title="Priority" color="#F4BD6B" />
+                {priorityAssignments.map((assignment) => (
+                    <Assignment
+                        key={assignment.name}
+                        name={assignment.name}
+                        course={assignment.course}
+                        due={assignment.dueDate}
+                        color={assignment.color}
+                    />
+                ))}
+                <SectionDivider title="Due Today" color="#7DC672" />
+                {dueTodayAssignments.map((assignment) => (
+                    <Assignment
+                        key={assignment.name}
+                        name={assignment.name}
+                        course={assignment.course}
+                        due={assignment.dueDate}
+                        color={assignment.color}
+                    />
+                ))}
+            </ol>
         </div>
-    )
-}
+    );
+};
 
 export default AssignmentsList;
--- app/settings/Course.tsx
+++ app/settings/Course.tsx
@@ -6,31 +6,7 @@ import { useState, useRef, useEffect } from "react";
 import useOnClickOutside from "../_hooks/useOnClickOutside";
 import Image from "next/image";
 
-import {
-  Slider,
-  Sketch,
-  Material,
-  Colorful,
-  Compact,
-  Circle,
-  Wheel,
-  Block,
-  Github,
-  Chrome,
-} from "@uiw/react-color";
-import {
-  Alpha,
-  Hue,
-  ShadeSlider,
-  Saturation,
-  Interactive,
-  hsvaToHslaString,
-} from "@uiw/react-color";
-import {
-  EditableInput,
-  EditableInputRGBA,
-  EditableInputHSLA,
-} from "@uiw/react-color";
+import { Sketch } from "@uiw/react-color";
 
 interface CourseProps {
   name: string;
@@ -42,16 +18,35 @@ const Course: React.FC<CourseProps> = ({ name, color }) => {
   const [colorPickerValue, setColorPickerValue] = useState(color);
   const colorPickerRef = useRef(null);
 
+  //Color Picker Functions
   const handleColorClick = () => {
     setIsColorPickerOpen(true);
   };
 
   const handleColorChange = (color: any) => {
-    console.log(color);
+    console.log(color.hex);
     setColorPickerValue(color.hex);
   };
 
   useOnClickOutside(colorPickerRef, () => setIsColorPickerOpen(false));
+
+  //Course name change functions
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [courseName, setCourseName] = useState(name);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCourseName(event.target.value);
+  };
+
+  const handleNameSubmit = () => {
+    // Here you would typically update the course name in your backend
+    setIsEditing(false);
+  };
 
   return (
     <>
@@ -60,21 +55,43 @@ const Course: React.FC<CourseProps> = ({ name, color }) => {
         <div className="p-2 flex  items-center w-full">
           <div
             className="w-6 h-6 rounded-full flex-shrink-0"
-            style={{ backgroundColor: color }}
+            style={{ backgroundColor: colorPickerValue }}
             onClick={handleColorClick}
           >
             {isColorPickerOpen && (
               <Sketch
                 style={{ marginLeft: 20 }}
-                color={colorPickerValue}
+                color={color}
                 onChange={handleColorChange}
                 ref={colorPickerRef}
               />
             )}
           </div>
           <div className="flex justify-between w-full">
-            <h4 className="text-xl font-semibold ml-2">{name}</h4>
-            <Image src={iconEdit} alt="edit" width={20} />
+            {/* <h4 className="text-xl font-semibold ml-2">{name}</h4> */}
+            {isEditing ? (
+              <input
+                type="text"
+                value={courseName}
+                onChange={handleNameChange}
+                onBlur={handleNameSubmit}
+                autoFocus
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    event.preventDefault(); // Prevent form submission
+                    handleNameSubmit(); // Call the function that disab les editing
+                  }
+                }}
+                className="text-xl font-semibold ml-2 w-full"
+              />
+            ) : (
+              <h4 className="text-xl font-semibold ml-2">{courseName}</h4>
+            )}
+            {!isEditing && (
+              <button onClick={handleEditClick}>
+                <Image src={iconEdit} alt="edit" width={20} />
+              </button>
+            )}
           </div>
         </div>
       </div>
