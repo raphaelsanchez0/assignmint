f526766bbe7ea9c1b1bb2f275df19cf474cb316f
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2023-12-28 19:49:56 -0500

    actions.ts now uses zod to check for empty and null strings

--- app/_actions/actions.ts
+++ app/_actions/actions.ts
@@ -3,14 +3,28 @@
 import { revalidatePath } from "next/cache";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "@/services/firebase";
+import { z } from "zod";
+
+const assignmentFormSchema = z.object({
+    course: z.string().min(1, "Course is required"),
+    title: z.string().min(1, "Title is required"),
+    dueDate: z.string().min(1, "Due date is required"),
+    notes: z.string().optional(),
+});
 
 export async function createAssignment(prevState: any, formData: FormData) {
     try {
-        await addDoc(collection(db, "assignments"), {
+        const parsedData = assignmentFormSchema.parse({
             course: formData.get("course"),
             title: formData.get("title"),
             dueDate: formData.get("due-date"),
             notes: formData.get("notes"),
+        });
+
+        const dueDate = new Date(parsedData.dueDate);
+        await addDoc(collection(db, "assignments"), {
+            ...parsedData,
+            dueDate,
         });
     } catch (e) {
         console.log(e);
