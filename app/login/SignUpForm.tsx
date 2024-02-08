ac69ca13e9a74cd8bfde2198c6b0594bd9547e7d
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-02-08 14:26:21 -0500

    feat: added email to signup form

---
+++ app/login/SignUpForm.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6, {
+      message: "Password is required.",
+    }),
+    confirm: z.string().min(6, {
+      message: "Password is required.",
+    }),
+  })
+  .refine((data) => data.confirm === data.password, {
+    message: "Password did not match",
+    path: ["confirm"],
+  });
+
+export default function SignUpForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirm: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <div className="grid gap-2">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="johndoe@gmail.com"
+                  {...field}
+                  type="email"
+                  onChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </Form>
+  );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,22 @@
 import { Card } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignInForm from "./SignInForm";
+import SignUpForm from "./SignUpForm";
 
 export default function AuthForm() {
   return (
     <Card className="w-full p-4">
-      <Tabs defaultValue="signin" className="w-full">
+      <Tabs defaultValue="signIn" className="w-full">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="signin">Sign In</TabsTrigger>
-          <TabsTrigger value="register">Register</TabsTrigger>
+          <TabsTrigger value="signIn">Sign In</TabsTrigger>
+          <TabsTrigger value="signUp">Sign Up</TabsTrigger>
         </TabsList>
-        <TabsContent value="signin">
+        <TabsContent value="signIn">
           <SignInForm />
         </TabsContent>
-        <TabsContent value="register">{/* <RegisterForm /> */}</TabsContent>
+        <TabsContent value="signUp">
+          <SignUpForm />{" "}
+        </TabsContent>
       </Tabs>
     </Card>
   );
