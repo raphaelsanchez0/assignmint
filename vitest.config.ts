df7e19828e0b7c51cdddf28797dd6f5b7bd56f37
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-02-17 16:15:21 -0500

    fix: Tests can use @ from importing components

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vitest/config";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",
