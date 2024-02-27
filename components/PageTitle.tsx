d19ab961a0db4caf6e659e8745d4363fc082a8c1
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-02-27 18:53:02 -0500

    feat: added dark mode toggle

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,5 +1,6 @@
 "use client";
 import date from "date-and-time";
+import { ModeToggle } from "./darkModeToggle/ModeToggle";
 
 interface PageTitleProps {
   title: string;
@@ -9,9 +10,14 @@ const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
   const date = getDateString();
 
   return (
-    <div className="p-8 flex flex-col w-full">
-      <h2 className="section-title">{title}</h2>
-      <h3 className="text-2xl font-semibold">{date}</h3>
+    <div className="flex align-middle p-8">
+      <div className="flex flex-col w-full">
+        <h2 className="section-title">{title}</h2>
+        <h3 className="text-2xl font-semibold">{date}</h3>
+      </div>
+      <div>
+        <ModeToggle />
+      </div>
     </div>
   );
 };
