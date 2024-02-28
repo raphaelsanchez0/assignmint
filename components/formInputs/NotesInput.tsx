a9e3cd398834e94825a059fa7a8fd36728b4504c
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-02-27 21:53:14 -0500

    fix: Dark mode input box color to black
    Adjusted the input box color to black in dark mode
    for enhanced readability and consistency

--- components/formInputs/NotesInput.tsx
+++ components/formInputs/NotesInput.tsx
@@ -14,7 +14,7 @@ const NotesInput: React.FC<NotesInputProps> = ({ edit, currentNotes }) => {
         name="notes"
         className="bg-slate-50 border border-gray-300 
                             text-gray-900 text-sm rounded-lg
-                            block w-full p-2.5 resize-none"
+                            block w-full p-2.5 resize-none dark:bg-zinc-900"
         placeholder="Notes"
         defaultValue={edit ? currentNotes : ""}
       />
