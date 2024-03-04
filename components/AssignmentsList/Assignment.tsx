b493f1cea31c081111f7c43402c8aa0b7c698b5d
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-03-03 20:32:00 -0500

    fix: Set more visible color to assignments onHover

--- components/AssignmentsList/Assignment.tsx
+++ components/AssignmentsList/Assignment.tsx
@@ -30,7 +30,7 @@ const Assignment: React.FC<AssignmentProps> = ({
     <>
       <hr className="h-px w-full bg-gray-400 border-0" />
       <Link href={`${path}?assignment=${id}`}>
-        <div className="h-16 flex flex-row w-full hover:bg-gray-100">
+        <div className="h-16 flex flex-row w-full hover:bg-gray-100 dark:hover:bg-zinc-800">
           <div className="w-1 h-full" style={{ backgroundColor: color }}></div>
           <div className="p-2 flex justify-between w-full">
             <div>
