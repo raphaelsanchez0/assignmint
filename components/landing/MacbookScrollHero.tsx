34ad39a504c365c1d5abfe9105e4ae007ce2392d
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-04-04 00:40:04 -0400

    feat: add mac-scroll

---
+++ components/landing/MacbookScrollHero.tsx
@@ -0,0 +1,9 @@
+import { MacbookScroll } from "../ui/macbook-scroll";
+
+export function MacbookScrollHero() {
+  return (
+    <div className="overflow-hidden dark:bg-[#0B0B0F] bg-white w-full">
+      <MacbookScroll />
+    </div>
+  );
+}
