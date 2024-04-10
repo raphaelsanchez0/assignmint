05c0d0d32020735cd3a70c01a166a33e682d38ee
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-04-10 02:02:35 -0400

    fix: call to action is centered

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -5,13 +5,14 @@ export default function Hero() {
   return (
     <>
       <div className="flex flex-col grow items-center justify-center">
-        <h1 className="font-bold text-5xl">Study Smarter, Not Harder</h1>
+        <h1 className="font-bold text-5xl text-center">
+          Study Smarter, Not Harder
+        </h1>
         <h2 className="text-3xl text-center">
           Streamline your academic life with intuitive tracking, automated
           reminders, and personalized study plans.
         </h2>
       </div>
-      <div className="h-2"></div>
     </>
   );
 }
