a812358d76be5e89cae5e906e7b20c641f1ba02f
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-04-12 01:49:27 -0400

    justify hero on in the center of the screen

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -4,11 +4,11 @@ import { TextGenerateEffect } from "../ui/text-generate-effect";
 export default function Hero() {
   return (
     <>
-      <div className="flex flex-col grow items-center justify-center">
-        <h1 className="font-bold text-5xl text-center">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="font-extrabold text-5xl text-center ">
           Study Smarter, Not Harder
         </h1>
-        <h2 className="text-3xl text-center">
+        <h2 className="text-3xl text-center font-light mt-6">
           Streamline your academic life with intuitive tracking, automated
           reminders, and personalized study plans.
         </h2>
