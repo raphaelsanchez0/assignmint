34d185d7a04fb09eb5d9ca944ca487e88dd6e796
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-04-15 17:58:01 -0400

    feat: create MacWindow component

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { TextGenerateEffect } from "../ui/text-generate-effect";
+import MacWindow from "./MacWindow";
 
 export default function Hero() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="font-extrabold text-5xl text-center ">
+      <div className="flex flex-col items-center justify-center h-full gap-8">
+        <h1 className="font-extrabold text-5xl text-center">
           Study Smarter, Not Harder
         </h1>
-        <h2 className="text-3xl text-center font-light mt-6">
+        <h2 className="text-3xl text-center font-light">
           Streamline your academic life with intuitive tracking, automated
           reminders, and personalized study plans.
         </h2>
+        <MacWindow />
       </div>
     </>
   );
