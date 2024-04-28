0353e128ad6099ec34e4ca2dc7516bd82348d648
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-04-28 16:01:42 -0400

    implement login button

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import { Card } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignInForm from "./SignInForm";
 import SignUpForm from "./SignUpForm";
+import { useSearchParams } from "next/navigation";
 
 export default function AuthForm() {
+  const router = useSearchParams();
+
+  const login = router.get("login");
+
+  const tab = login === "true" ? "signIn" : "signUp";
+
   return (
     <Card className="w-full p-4">
-      <Tabs defaultValue="signUp" className="w-full">
+      <Tabs defaultValue="signUp" className="w-full" value={tab}>
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="signUp">Sign Up</TabsTrigger>
           <TabsTrigger value="signIn">Sign In</TabsTrigger>
--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -19,15 +19,22 @@ export default function Navbar() {
           AssignMint
         </h1>
       </a>
-      <Link href="/login">
-        <button
-          type="button"
-          className="bg-gradient-to-r from-teal-500 to-green-500 rounded-full px-6 py-2 text-white font-semibold shadow-md hover:shadow-lg transition duration-200 ease-in-out
+      <div className="flex">
+        <Link href="/login?login=true">
+          <button className="px-6 py-2 font-semibold hidden lg:block">
+            Sign In
+          </button>
+        </Link>
+        <Link href="/login">
+          <button
+            type="button"
+            className="bg-gradient-to-r from-teal-500 to-green-500 rounded-full px-6 py-2 text-white font-semibold shadow-md hover:shadow-lg transition duration-200 ease-in-out
           hidden lg:block"
-        >
-          Sign Up
-        </button>
-      </Link>
+          >
+            Sign Up
+          </button>
+        </Link>
+      </div>
     </nav>
   );
 }
