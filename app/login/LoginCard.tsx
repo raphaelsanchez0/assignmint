301566cdae5332c3a3dbae12ed1b410e12437683
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-04-29 00:50:50 -0400

    fix login bug

--- app/login/LoginCard.tsx
+++ app/login/LoginCard.tsx
@@ -1,19 +1,27 @@
 import { Card } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import SignUpForm from "./SignUpForm";
 import SignInForm from "./SignInForm";
 
 export default function LoginCard() {
+  const [activeTab, setActiveTab] = React.useState("signUp");
   const router = useSearchParams();
+  useEffect(() => {
+    const login = router.get("login");
 
-  const login = router.get("login");
+    if (login === "true") setActiveTab("signIn");
+  }, []);
 
-  const tab = login === "true" ? "signIn" : "signUp";
   return (
     <Card className="w-full p-4">
-      <Tabs defaultValue="signUp" className="w-full" value={tab}>
+      <Tabs
+        defaultValue="signUp"
+        className="w-full"
+        value={activeTab}
+        onValueChange={setActiveTab}
+      >
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="signUp">Sign Up</TabsTrigger>
           <TabsTrigger value="signIn">Sign In</TabsTrigger>
