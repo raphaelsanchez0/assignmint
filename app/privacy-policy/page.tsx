760f9a2a27fd6ee798cb3d92e429e4dbc25298f6
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-05-03 02:06:36 -0400

    fix: escape characters fixed in privacy policy

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -56,23 +56,24 @@ export default function Policy() {
       <p>
         We want to inform you that whenever you visit our Service, we collect
         information that your browser sends to us that is called Log Data. This
-        Log Data may include information such as your computer's Internet
-        Protocol ("IP") address, browser version, pages of our Service that you
-        visit, the time and date of your visit, the time spent on those pages,
-        and other statistics.
+        Log Data may include information such as your computer&apos;s Internet
+        Protocol (&quot;IP&quot;) address, browser version, pages of our Service
+        that you visit, the time and date of your visit, the time spent on those
+        pages, and other statistics.
       </p>
       <br />
 
       <h2 className={sectionHeaderStyle}>Cookies</h2>
       <p>
-        Cookies are files with small amount of data that is commonly used an
-        anonymous unique identifier. These are sent to your browser from the
-        website that you visit and are stored on your computer's hard drive.
+        Cookies are files with a small amount of data that is commonly used as
+        an anonymous unique identifier. These are sent to your browser from the
+        website that you visit and are stored on your computer&apos;s hard
+        drive.
       </p>
       <br />
       <p>
-        Our website uses these "cookies" to collection information and to
-        improve our Service. You have the option to either accept or refuse
+        Our website uses these &quotcookies&quot to collection information and
+        to improve our Service. You have the option to either accept or refuse
         these cookies, and know when a cookie is being sent to your computer. If
         you choose to refuse our cookies, you may not be able to use some
         portions of our Service.
@@ -120,7 +121,7 @@ export default function Policy() {
         practices of any third-party sites or services.
       </p>
       <br />
-      <p>Children's Privacy</p>
+      <p>Children&aposs Privacy</p>
       <p>
         Our Services do not address anyone under the age of 13. We do not
         knowingly collect personal identifiable information from children under
@@ -139,14 +140,7 @@ export default function Policy() {
         any changes by posting the new Privacy Policy on this page. These
         changes are effective immediately, after they are posted on this page.
       </p>
-      <br />
-      <p>
-        Our Privacy Policy was created with the help of the{" "}
-        <a href="https://www.privacypolicytemplate.net">
-          Privacy Policy Template
-        </a>
-        .
-      </p>
+
       <br />
 
       <h2 className={sectionHeaderStyle}>Contact Us</h2>
