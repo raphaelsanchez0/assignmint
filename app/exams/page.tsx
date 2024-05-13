9a11db466025aea0e7abbb49490d6b82dfc6acc2
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-05-12 21:39:31 -0400

    feat: exams and assignments are responsive

--- app/exams/page.tsx
+++ app/exams/page.tsx
@@ -10,13 +10,16 @@ import ExamsCard from "@/components/ExamsList/ExamsCard";
 export default async function Exams() {
   return (
     <>
-      <div className="ml-sidebar-width">
+      <div className="mb-sidebar-width md:ml-sidebar-width">
         <div className="flex gap-4 p-4">
-          <div className="basis-1/2 ">
+          <div className="basis-1/2 hide-when-mobile">
             <ExamsCard />
           </div>
-          <div className="basis-1/2">
+          <div className="basis-1/2 hide-when-mobile">
             <AddExam />
+          </div>
+          <div className="grow md:hidden">
+            <ExamsCard showAddExam />
           </div>
         </div>
       </div>
