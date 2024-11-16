376e7851a0978677d3a5bd2d2211c030798624d6
Raphael Sanchez <sanchezraphael0@gmail.com>
2024-11-15 22:43:45 -0500

    style: fix styling of button

--- app/import/page.tsx
+++ app/import/page.tsx
@@ -21,9 +21,10 @@ export default function ImportAssignments() {
       </h2>
       <div className="flex gap-3">
         <Card className="basis-1/2 sm:p-4 md:p-6">
-          <CardTitle className="pb-2 text-center">
-            Assignments from Canvas
-          </CardTitle>
+          <div className="flex items-center justify-between mb-4">
+            <h3 className="card-title">Assignments From Canvas</h3>
+            <button className="btn">Add</button>
+          </div>
           {assignments &&
             Object.entries(assignments).map(([date, assignments], index) => (
               <Date key={index} date={date} assignments={assignments} />
