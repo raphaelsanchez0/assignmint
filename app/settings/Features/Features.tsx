5c1d4ba4bdcb4b5a29c43c012edd5ac7652b8e46
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2025-10-01 00:24:12 -0400

    update changelog

--- app/settings/Features/Features.tsx
+++ app/settings/Features/Features.tsx
@@ -6,10 +6,9 @@ export default function Features() {
     <Card className="flex flex-col gap-2">
       <CardTitle>Changelog + Upcoming</CardTitle>
       <CardDescription>
-        Here are some features that I am working on:
+        Here are some features that we am working on:
       </CardDescription>
       <ul className="list-disc pl-5">
-        <li>Progress slider for assignments</li>
         <li>Detailed Assignment/Exam history page</li>
         <li>
           Day in calendar page uses colors on day to show if an event is
@@ -18,11 +17,12 @@ export default function Features() {
       </ul>
       <CardDescription>Where is what we just added:</CardDescription>
       <ul className="list-disc pl-5">
+        <li>Progress slider for assignments - 9/30/25</li>
         <li>
           "Completed Today" under Assignments in Dashboard and restore
-          assignment feature - 9/24
+          assignment feature - 9/24/25
         </li>
-        <li>Fix: View Assignments/Exams popup shows notes - 9/10</li>
+        <li>Fix: View Assignments/Exams popup shows notes - 9/10/25</li>
       </ul>
       <CardDescription>
         Please bear with us, we are just two students who work on this in our
